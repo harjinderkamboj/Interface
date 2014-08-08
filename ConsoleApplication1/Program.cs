538b0df756df005e3e5efa33adcec6861f6d5c68
Harjinder Kamboj <harjinder.kamboj@gmail.com>
2014-08-08 15:08:21 -0400

    First Commit for project upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+	
+
+namespace ConsoleApplication1
+{
+    class Program 
+    {
+		private static IRService inf;
+
+
+		public Program(IRService inf)
+		{
+			inf = new RService();
+		}
+
+		static void Main(string[] args)
+        {
+			inf = new RService();
+			Console.WriteLine(inf.AddOne(1));
+			Console.ReadLine();
+
+        }
+    }
+}
