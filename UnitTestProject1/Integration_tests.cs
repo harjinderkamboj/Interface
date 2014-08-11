e4e04735427f940127912228632cfcafbd7015bb
Harjinder <harjinder.kamboj@gmail.com>
2014-08-11 19:02:58 -0400

    added integration test

---
+++ UnitTestProject1/Integration_tests.cs
@@ -0,0 +1,28 @@
+﻿using Castle.Windsor;
+using Castle.Windsor.Installer;
+using ClassLibrary1;
+using Dal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Integration_tests
+    {
+
+        [TestMethod]
+        public void Test_Person_Repository()
+        {
+
+            IPersonRepository p = new PersonRepository();
+
+            Assert.AreEqual("You requested 2", p.getById(2));
+        }
+
+
+    }
+
+
+}
---
+++ UnitTestProject1/Tests_ClassLibrary.cs
@@ -0,0 +1,31 @@
+﻿using Castle.Windsor;
+using Castle.Windsor.Installer;
+using ClassLibrary1;
+using Dal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using System;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class Tests_ClassLibrary
+	{
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+			var mockPersonRepository = MockRepository.GenerateMock<IPersonRepository>();
+			mockPersonRepository.Stub(x => x.getById(2)).Return("You asked 2");
+
+			var i = new RService(mockPersonRepository);
+
+			Assert.AreEqual("You asked 2", i.GetDataFromPR(2));
+		}
+
+	
+	}
+
+
+}
